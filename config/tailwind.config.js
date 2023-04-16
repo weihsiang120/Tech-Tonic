bcf8bb1bc27cd44cb075b67406f14b553349475a
Andrew77498864 <andrew77498864@gmail.com>
2023-04-17 00:37:05 +0800

    set project color palette in tailwind.config.js

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,35 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+    colors: {
+      blue: {
+        base: "#f3f4fe",
+        light: "#d1defc",
+        main: "#6380f2",
+        dark: "#2c3085",
+      },
+      gray: {
+        light: "#aab1cc",
+        dark: "#6a6e81",
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
