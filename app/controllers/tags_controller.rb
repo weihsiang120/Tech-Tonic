be175437642795011c6853de12175920ef8d01b3
Andrew77498864 <andrew77498864@gmail.com>
2023-04-26 11:11:07 +0800

    add follow tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,13 @@
 class TagsController < ApplicationController
+  before_action :authenticate_user!, only: %i[ follow ]
   def show
-    @tag = Tag.find_by(id: params[:id])
+    @tag = Tag.find(params[:id])
     @posts = @tag.posts
+  end
+
+  def follow
+    @tag = Tag.find(params[:id])
+    current_user.tags << @tag if !current_user.tags.include?(@tag)
+    redirect_to tag_path(@tag.id), notice: "已追蹤#{@tag.name}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :posts
   get "tags/:id", to: "tags#show", as: "tag"
+  get "tags/:id/follow", to: "tags#follow", as: "tag_follow"
   root to: 'posts#index'
 
 end
