3bcdbe3d22b1b1b195bd7320a72c0b47289a2484
Andrew77498864 <andrew77498864@gmail.com>
2023-04-26 15:20:33 +0800

    add post soft-delete

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :post_tags
   has_many :tags, through: :post_tags
+
+  default_scope { where(deleted_at: nil) }
+  
+  def destroy
+    update(deleted_at: Time.current)
+  end
 end
