9a2aaecfb5959d91f5bff0a1b9a4cedf6f2ed276
Andrew77498864 <andrew77498864@gmail.com>
2023-04-26 14:27:25 +0800

    delete duplicated migration

--- db/migrate/20230425031242_create_posts.rb
+++ db/migrate/20230425031242_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.string :title
       t.text :content
+      t.string :status, default: "draft"
       t.datetime :deleted_at
       t.belongs_to :user, null: false, foreign_key: true
 
