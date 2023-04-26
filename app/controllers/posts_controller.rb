4a39469ae08e0f789904f70ebbec988defab9bfa
Andrew77498864 <andrew77498864@gmail.com>
2023-04-26 15:13:14 +0800

    add post tags editable

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :find_post, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, only: %i[ new edit create update destroy ]
   # GET /posts or /posts.json
   def index
@@ -28,20 +28,8 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    # content = post_params[:text]
-    # lines = content.split("\n")
-
-    # title = ""
-    # lines.each do |line|
-    #   if line.start_with? "# "
-    #     title = line.sub("# ", "")
-    #   end
-    # end
-
-    # if title == ""
-    #   title = lines[0]
-    # end
-    get_post_title
+    
+    set_post_title
     @post = Post.new(title: @title, content: @content, user_id: current_user.id)
     add_tags_to_post
 
@@ -54,9 +42,11 @@ class PostsController < ApplicationController
 
   def update
 
-    get_post_title
+    @post.tags.clear
+    set_post_title
+    add_tags_to_post
 
-    if @post.update(post_params)
+    if @post.update(title: @title, content: @content, tags: @post.tags)
       redirect_to posts_url, notice: "Post was successfully updated."
     else
       render :edit, status: :unprocessable_entity
@@ -72,7 +62,7 @@ class PostsController < ApplicationController
 
   private
   # Use callbacks to share common setup or constraints between actions.
-  def set_post
+  def find_post
     @post = Post.find(params[:id])
   end
 
@@ -81,7 +71,7 @@ class PostsController < ApplicationController
     params.require(:post).permit(:content, :tag_list, :status)
   end
 
-  def get_post_title
+  def set_post_title
     @title = ""
 
     @content = post_params[:content]
@@ -101,11 +91,10 @@ class PostsController < ApplicationController
 
   def add_tags_to_post
     if !post_params[:tag_list].empty?
-      tag_list = post_params[:tag_list]
-      tag_list = tag_list.split(", ")
+      tag_list = post_params[:tag_list].split(",")
 
       tag_list.each do |tag_name|
-        tag = Tag.find_or_create_by(name: tag_name.downcase)
+        tag = Tag.find_or_create_by(name: tag_name.downcase.strip.squish.gsub(/[^0-9A-Za-z]/,"_"))
         @post.tags << tag
       end
     end
