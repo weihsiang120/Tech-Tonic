1e05ff9f19125f7ee4cf03d7fec8267c1a4773f1
Andrew77498864 <andrew77498864@gmail.com>
2023-04-26 15:27:38 +0800

    add user unfollow tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,3 +1,4 @@
+
 class TagsController < ApplicationController
   before_action :authenticate_user!, only: %i[ follow ]
   def show
@@ -7,7 +8,14 @@ class TagsController < ApplicationController
 
   def follow
     @tag = Tag.find(params[:id])
-    current_user.tags << @tag if !current_user.tags.include?(@tag)
-    redirect_to tag_path(@tag.id), notice: "已追蹤#{@tag.name}"
+
+    if !current_user.tags.include?(@tag)
+      current_user.tags << @tag
+      redirect_to tag_path(@tag.id), notice: "已追蹤#{@tag.name}"
+    else
+      current_user.tags.delete(@tag)
+      redirect_to tag_path(@tag.id), notice: "已退追#{@tag.name}"
+    end
+     
   end
 end
