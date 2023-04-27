742b4e106dd38c59109f3df4dcd1f6bc01b5a991
Andrew Fei <andrew77498864@gmail.com>
2023-04-27 23:01:30 +0800

    update home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-   @posts = Post.all.order(created_at: :desc).page(params[:page])
+    require "time"
+    @posts = Post.all.order(created_at: :desc).page(params[:page])
   end
 
 end
