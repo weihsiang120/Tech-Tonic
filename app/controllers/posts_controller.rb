7aeefa1442abd03fee9bc250b72c2ce4a22360f6
Andrew Fei <andrew77498864@gmail.com>
2023-04-29 21:47:38 +0800

    WIP - add user personal page and fix navbar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -67,7 +67,7 @@ class PostsController < ApplicationController
   end
 
   def find_posts
-    @post = current_user.posts.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def add_tags_to_post
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: "users/registrations" }
   
-
   resources :posts do
     resources :comments
   end
   
   get "tags/:id", to: "tags#show", as: "tag"
-  get "tags/:id/follow", to: "tags#follow", as: "tag_follow"
+  put "tags/:id/follow", to: "tags#follow", as: "tag_follow"
 
   root to: 'home#index'
-
 end
