01f9738ccbadb4d2aee3fd2a272e6d61d82a63d2
AhernShaia <gemg4gtii@gmail.com>
2023-04-30 22:56:09 +0800

    文章閱讀頁面切版

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,7 +14,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 end
   def github
-    
+
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
@@ -25,8 +25,8 @@ end
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
   end
-end
 
 
- 
-end
+
+
+
