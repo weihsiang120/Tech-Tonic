ae6114df3027df34db833ec53ed3895ccd55e8eb
Wei Hsiang Hsiao <weihsiang120@gmail.com>
2023-05-05 16:36:57 +0800

    Update application_record.rb

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
-
-  # def self.search(keyword)
-  #   where("title like ? or content like ?", "%#{keyword}%", "%#{keyword}%")
-  # end
 end
