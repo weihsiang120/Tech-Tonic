9ccf25e29b8599d3bb2701ffe4c1a7f3d377bf3f
weihsiang120 <weihsiang120@gmail.com>
2023-05-10 11:55:46 +0800

    tag show頁 修正搜尋功能

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,19 @@
 
 class TagsController < ApplicationController
   before_action :authenticate_user!, only: %i[ follow user_tags ]
+
   def show
     @tag = Tag.find(params[:id])
     @tags = Tag.all
-    @posts = @tag.posts.order(sort_order).page(params[:page])
+    if params[:keyword].present?
+      @posts = Post.published.search(params[:keyword]).order(created_at: :desc).page(params[:page])
+      if @posts.empty?
+        flash.now[:notice] = "No results found for '#{params[:keyword]}'"
+        @posts = Post.published.order(created_at: :desc).page(params[:page])
+      end
+    else
+      @posts = Post.published.order(created_at: :desc).page(params[:page])
+    end
   end
 
   def follow
