5ee79d8f611af833ce5a78b6a9419f3def968ebc
weihsiang120 <weihsiang120@gmail.com>
2023-05-10 12:41:48 +0800

    增加我的文章搜尋功能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,15 @@ class UsersController < ApplicationController
     if current_user == @user
       redirect_to edit_user_registration_path
     end
-    @posts = @user.posts.published.order(created_at: :desc).page(params[:page])
+    if params[:keyword].present?
+      @posts = Post.published.search(params[:keyword]).order(created_at: :desc).page(params[:page])
+      if @posts.empty?
+        flash.now[:notice] = "No results found for '#{params[:keyword]}'"
+        @posts = Post.published.order(created_at: :desc).page(params[:page])
+      end
+    else
+      @posts = Post.published.order(created_at: :desc).page(params[:page])
+    end
   end
 
   def follow
