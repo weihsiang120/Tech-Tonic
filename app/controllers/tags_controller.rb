531d24e14960c3564402a9eb1b5aeaaceb66983e
weihsiang120 <weihsiang120@gmail.com>
2023-05-11 17:43:57 +0800

    修正點讚、搜尋功能和調整我的標籤頁面

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,16 +3,16 @@ class TagsController < ApplicationController
   before_action :authenticate_user!, only: %i[ follow user_tags ]
 
   def show
-    @tag = Tag.find(params[:id])
+    @tag = Tag.includes(:posts).find(params[:id])
     @tags = Tag.all
     if params[:keyword].present?
-      @posts = Post.published.search(params[:keyword]).order(created_at: :desc).page(params[:page])
+      @posts = @tag.posts.published.search(params[:keyword]).order(created_at: :desc).page(params[:page])
       if @posts.empty?
         flash.now[:notice] = "No results found for '#{params[:keyword]}'"
-        @posts = Post.published.order(created_at: :desc).page(params[:page])
+        @posts = @tag.posts.published.order(created_at: :desc).page(params[:page])
       end
     else
-      @posts = Post.published.order(created_at: :desc).page(params[:page])
+      @posts = @tag.posts.published.order(created_at: :desc).page(params[:page])
     end
   end
 
